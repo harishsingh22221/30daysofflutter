646c2e0bbda871b52de3e7d965af1af96e347bf7
harish singh <harishsingh22221@gmail.com>
2021-07-20 23:29:03 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "harish";
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days India by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutappe/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,14 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to India"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
